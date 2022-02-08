13c491ab6dc75f42a6af71b3115639cf1726caac
micrab001 <micrab@bk.ru>
2022-02-08 18:22:23 +0300

    пока остановился на таком варианте

--- vipiski.py
+++ vipiski.py
@@ -11,7 +11,7 @@ filesnames = [dirname+chr(92)+f for f in all_dir if os.path.isfile(dirname+chr(9
 
 # задание некоторых начальных переменных
 all_data = ""
-filename_xls = dirname+chr(92)+"all_data.xlsx"
+filename_xls = dirname+chr(92)+"all_data_"+dirname.split("\\")[-1]+".xlsx"
 strt_time = datetime.date(1900, 1, 1)  # объект для конвертации даты
 
 
@@ -79,37 +79,51 @@ all_data = "[" + all_data + "]"
 
 with open("test.json", "w") as tst_file:
     tst_file.write(all_data)
+print("Выписки обработаны")
 
 # начало работы с данными
 # считывание выписок
 df = pd.DataFrame(json.loads(all_data))
 # создание выборки данных из выписок по эквайрингу
-svod = df[["Дата","Сумма","ДатаПоступило","Получатель","НазначениеПлатежа","Номер мерчанта",
-           "Дата операции магазин","Сумма комиссии","Возврат1","Возврат2"]]
+svod = df[["Дата", "Сумма", "ДатаПоступило", "Получатель", "НазначениеПлатежа", "Номер мерчанта",
+           "Сумма комиссии", "Возврат1", "Возврат2"]] # "Дата операции магазин"
 svod = svod[svod["Номер мерчанта"].notnull()]
-svod = svod.sort_values(by=["Номер мерчанта","Дата"])
+svod = svod.sort_values(by=["Номер мерчанта", "Дата"])
 svod["Получено"] = svod["Сумма"] + svod["Сумма комиссии"]
 
+print("таблица выписок сформирована")
+
 # считываение данных по эквайрингу от Сбера
-filename = filedialog.askopenfilename(initialdir= os.getcwd())
+filename = filedialog.askopenfilename(initialdir=os.getcwd())
 sber_df = pd.read_excel(pd.ExcelFile(filename), "Sheet0")
+print("чтение данных Сбер завершено")
 sber_df["Дата зачисления"] = sber_df["Дата выгрузки в АБС"].apply(convert_data)
+sber_df["Дата зачисления"] = sber_df["Дата зачисления"].astype("str").apply(str_to_data)
+sber_df["Дата операции магазин"] = sber_df["Дата операции магазин"].apply(convert_data)
+sber_df["Дата операции магазин"] = sber_df["Дата операции магазин"].astype("str").apply(str_to_data)
 # Номер мерчанта, Дата операции магазин, Дата выгрузки в АБС, Сумма операции, Сумма комиссии, Сумма расчета
 sber_df["Номер мерчанта"] = sber_df["Номер мерчанта"].astype("str")
 # получение выборки данных из эквайринга Сбера
 svod_sber_df = sber_df[["Номер мерчанта", "Дата зачисления", "Сумма операции", "Сумма комиссии", "Сумма расчета"]]
 svod_sber_df = svod_sber_df.groupby(["Номер мерчанта", "Дата зачисления"], as_index=False).sum() #[["Сумма операции"]]
-svod_sber_df["Дата зачисления"] = svod_sber_df["Дата зачисления"].astype("str").apply(str_to_data)
+# дополнительный расчет по сберу
+sber_test = sber_df[["Номер мерчанта", "Дата зачисления", "Дата операции магазин", "Сумма операции", "Сумма комиссии", "Сумма расчета"]]
+sber_test = sber_test.groupby(["Номер мерчанта", "Дата зачисления", "Дата операции магазин"], as_index=False).sum()
+
 # слияние данных сбера и банка
 itog = svod_sber_df.merge(svod, how = "left", left_on= ["Номер мерчанта", "Дата зачисления"], right_on=["Номер мерчанта", "Дата"],
           suffixes=('_sber', '_bank'))
+
 # проверка на пустые значения после слияния
 for i in range(1, len(itog)):
     if pd.isna(itog.loc[i, "Получено"]):
         itog.loc[i, "Получено"] = 0
         itog.loc[i, "Возврат1"] = 0
         itog.loc[i, "Возврат2"] = 0
-itog["Проверка"] = itog["Сумма операции"] - itog["Получено"] - itog["Возврат1"] - itog["Возврат2"]
+        itog.loc[i, "Сумма комиссии_bank"] = 0
+        itog.loc[i, "Сумма"] = 0
+
+itog["Проверка"] = itog["Сумма операции"] - itog["Получено"] # - itog["Возврат1"] - itog["Возврат2"]
 itog["Накопительно"] = 0
 itog.loc[1, "Накопительно"] =  itog.loc[1, "Проверка"]
 for i in range(2, len(itog)):
@@ -118,6 +132,15 @@ for i in range(2, len(itog)):
     else:
         itog.loc[i, "Накопительно"] = itog.loc[i, "Проверка"]
 
+# собирает итоги месяца по мерчанту, если проверка не равна 0 надо искать потерянные платежи
+itog_sum = itog[["Номер мерчанта", "Проверка"]]
+itog_sum = itog_sum.groupby(["Номер мерчанта"], as_index=False).sum()
+# поиск не найденных платежей по совпадению сумм
+list_plat = itog_sum[itog_sum["Проверка"]!= 0]["Проверка"].tolist()
+find_plat =  svod[svod["Получено"].isin(list_plat)]
+#
+#
+print("расчеты произведены")
 # print (itog)
 # exit("my stop")
 
@@ -130,7 +153,7 @@ def write_worksheet(panda_df, name_worksheet):
         if "ДАТА" in col.upper():
             worksheet.set_column(count, count, 15, format_data)
         count += 1
-
+    print("формирование листа ", name_worksheet)
 
 # запись данных в эксель
 writer = pd.ExcelWriter(filename_xls, engine='xlsxwriter')
@@ -141,6 +164,11 @@ with writer as file_name:
     write_worksheet(svod, "Эквайринг банк")
     write_worksheet(svod_sber_df, "Эквайринг сбер")
     write_worksheet(itog, "Проверка")
+    write_worksheet(itog_sum, "Проверка месяц")
+    write_worksheet(find_plat, "Не найденные платежи")
+    write_worksheet(sber_test, "Сбер операции в магазине")
+print("запись завершена")
+
 
 
 
